ade1266d549ac40334721f579ab0ec36b206025b
Aman <rjaman330@gmail.com>
2021-05-06 13:54:56 +0530

    connecting online databse

---
+++ server1.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3300
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
+const Emitter = require('events')
+
+
+
